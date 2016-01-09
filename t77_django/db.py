776821069d69187a997febc24f8672611b66c083
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-01-09 21:57:33 +0100

    Add using param to pg_version

--- t77_django/db.py
+++ t77_django/db.py
@@ -1,8 +1,15 @@
-from django.db import connection
+from django.db.transaction import get_connection
 
 
-def pg_version():
-    """Return PostgreSQL version"""
+def pg_version(using=None):
+    """
+    Return tuple with PostgreSQL version of a specific connection
+    :type using: str
+    :param using: Connection name
+    :rtype: tuple
+    :return: PostgreSQL version
+    """
+    connection = get_connection(using)
     cursor = connection.cursor()
 
     cursor.execute('SHOW server_version')
--- tests/db_test.py
+++ tests/db_test.py
@@ -7,32 +7,36 @@ except ImportError:
 from t77_django.db import pg_version
 
 
-class MockCursor(object):
-    def __init__(self, version):
-        self.version = version
+def mock_get_connection(version):
+    class MockCursor(object):
+        def __init__(self, _version):
+            self.version = _version
 
-    def execute(self, *args):
-        pass
+        def execute(self, *args):
+            pass
 
-    def fetchone(self):
-        return [self.version]
+        def fetchone(self):
+            return [self.version]
 
+    class MockConnection(object):
+        def __init__(self, _version):
+            self.version = _version
 
-class MockConnection(object):
-    def __init__(self, version):
-        self.version = version
+        def cursor(self):
+            return MockCursor(self.version)
 
-    def cursor(self):
-        return MockCursor(self.version)
+    def get_connection(using):
+        return MockConnection(version)
+    return get_connection
 
 
 class PgVersionTestCase(unittest.TestCase):
-    @mock.patch('t77_django.db.connection', MockConnection('9.4.1'))
+    @mock.patch('t77_django.db.get_connection', mock_get_connection('9.4.1'))
     def test_9_4_1(self):
         """Version 9.4.1"""
         self.assertEqual(pg_version(), (9, 4, 1))
 
-    @mock.patch('t77_django.db.connection', MockConnection('9.1.2'))
+    @mock.patch('t77_django.db.get_connection', mock_get_connection('9.1.2'))
     def test_9_1_2(self):
         """Version 9.1.2"""
         self.assertEqual(pg_version(), (9, 1, 2))
