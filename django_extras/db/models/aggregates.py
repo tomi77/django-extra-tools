a28bca577980fd2105e876e5e51f4019526c8051
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-22 14:27:26 +0100

    pep8 fix

--- django_extras/db/models/aggregates.py
+++ django_extras/db/models/aggregates.py
@@ -5,15 +5,14 @@ from django.db.models import Aggregate as BaseAggregate
 
 from .sql import aggregates as base_aggregates_module
 
-__all__ = ['First', 'Last', 'Median', 'StringAgg']
-
 
 class Aggregate(BaseAggregate):
     aggregates_module = base_aggregates_module
 
     def add_to_query(self, query, alias, col, source, is_summary):
         klass = getattr(self.aggregates_module, self.name)
-        aggregate = klass(col, source=source, is_summary=is_summary, **self.extra)
+        aggregate = klass(col, source=source, is_summary=is_summary,
+                          **self.extra)
         query.aggregates[alias] = aggregate
 
 
