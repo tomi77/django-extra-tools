b6758d8de5e48091633457ae5d5ee7c69c11061e
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-07-11 12:30:08 +0200

    new function: batch_qs

--- django_extras/db/__init__.py
+++ django_extras/db/__init__.py
@@ -16,3 +16,22 @@ def pg_version(using=None):
     row = cursor.fetchone()
 
     return tuple([int(i) for i in row[0].split('.')])
+
+
+def batch_qs(qs, batch_size=1000):
+    """
+    Returns a (start, end, total, queryset) tuple for each batch in the given
+    queryset.
+
+    Usage:
+        # Make sure to order your querset
+        article_qs = Article.objects.order_by('id')
+        for start, end, total, qs in batch_qs(article_qs):
+            print "Now processing %s - %s of %s" % (start + 1, end, total)
+            for article in qs:
+                print article.body
+    """
+    total = qs.count()
+    for start in range(0, total, batch_size):
+        end = min(start + batch_size, total)
+        yield (start, end, total, qs[start:end])
