78ec6ae309ae97f74a5da5acccbb93304c799099
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-07-11 19:34:37 +0200

    fix test config

--- testapp/settings.py
+++ testapp/settings.py
@@ -1,13 +1,16 @@
+import os
+
+
 SECRET_KEY = 'qaz123'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', 'travis'),
+        'USER': os.environ.get('DATABASE_USER', 'travis'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
     }
 }
 
