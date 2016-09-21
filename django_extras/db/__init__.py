d8941a38b947ee3e9c23b1243dbb2652c7800b55
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-09-21 18:57:21 +0200

    typo

--- django_extras/db/__init__.py
+++ django_extras/db/__init__.py
@@ -32,7 +32,7 @@ def batch_qs(qs, batch_size=1000):
     queryset.
 
     Usage:
-        # Make sure to order your querset
+        # Make sure to order your queryset
         article_qs = Article.objects.order_by('id')
         for start, end, total, qs in batch_qs(article_qs):
             print "Now processing %s - %s of %s" % (start + 1, end, total)
