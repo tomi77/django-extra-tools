08bfde3f18b5cc08fadc1d05d93571d8e124aea8
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-09-21 21:52:55 +0200

    fix tests

--- testapp/tests.py
+++ testapp/tests.py
@@ -36,12 +36,12 @@ def mock_get_connection(version):
 
 
 class PgVersionTestCase(TestCase):
-    @mock.patch('django_extras.db.get_connection', mock_get_connection('9.4.1'))
+    @mock.patch('django_extra_tools.db.get_connection', mock_get_connection('9.4.1'))
     def test_9_4_1(self):
         """Version 9.4.1"""
         self.assertEqual(pg_version(), (9, 4, 1))
 
-    @mock.patch('django_extras.db.get_connection', mock_get_connection('9.1.2'))
+    @mock.patch('django_extra_tools.db.get_connection', mock_get_connection('9.1.2'))
     def test_9_1_2(self):
         """Version 9.1.2"""
         self.assertEqual(pg_version(), (9, 1, 2))
