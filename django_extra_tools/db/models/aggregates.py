4e5b7ec4a6961fc0015e2b7355fcc2137b51f09e
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-04-12 19:23:42 +0200

    fix Aggregate for django >= 1.10

--- django_extra_tools/db/models/aggregates.py
+++ django_extra_tools/db/models/aggregates.py
@@ -1,19 +1,25 @@
 """
 Classes to represent the definitions of aggregate functions.
 """
-from django.db.models import Aggregate as BaseAggregate
+import django
 
-from .sql import aggregates as base_aggregates_module
+if django.VERSION[:2] <= (1, 7):
+    from django.db.models import Aggregate as BaseAggregate
+
+    from .sql import aggregates as base_aggregates_module
 
 
-class Aggregate(BaseAggregate):
-    aggregates_module = base_aggregates_module
+    class Aggregate(BaseAggregate):
+        aggregates_module = base_aggregates_module
 
-    def add_to_query(self, query, alias, col, source, is_summary):
-        klass = getattr(self.aggregates_module, self.name)
-        aggregate = klass(col, source=source, is_summary=is_summary,
-                          **self.extra)
-        query.aggregates[alias] = aggregate
+        def add_to_query(self, query, alias, col, source, is_summary):
+            klass = getattr(self.aggregates_module, self.name)
+            aggregate = klass(col, source=source, is_summary=is_summary,
+                              **self.extra)
+            query.aggregates[alias] = aggregate
+        pass
+else:
+    from django.db.models import Aggregate
 
 
 class AggregateWithOrderBy(Aggregate):
