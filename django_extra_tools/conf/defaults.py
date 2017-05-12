3d96c77eab14be6c7d3e551aa6e323ff67fe1eb1
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-05-12 20:18:18 +0200

    Refactoring XHR middleware

--- django_extra_tools/conf/defaults.py
+++ django_extra_tools/conf/defaults.py
@@ -2,3 +2,9 @@
 
 # auth.backends.SuperUserAuthenticateMixin username separator
 AUTH_BACKEND_USERNAME_SEPARATOR = ':'
+
+XHR_MIDDLEWARE_ALLOWED_ORIGINS = '*'
+XHR_MIDDLEWARE_ALLOWED_METHODS = ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
+XHR_MIDDLEWARE_ALLOWED_HEADERS = ['Content-Type', 'Authorization', 'Location', '*']
+XHR_MIDDLEWARE_ALLOWED_CREDENTIALS = 'true'
+XHR_MIDDLEWARE_EXPOSE_HEADERS = ['Location']
--- django_extra_tools/middleware.py
+++ django_extra_tools/middleware.py
@@ -1,13 +1,6 @@
 from django import http
-from django.conf import settings
 
-XHR_MIDDLEWARE_ALLOWED_ORIGINS = getattr(settings, 'XHR_MIDDLEWARE_ALLOWED_ORIGINS', '*')
-XHR_MIDDLEWARE_ALLOWED_METHODS = getattr(settings, 'XHR_MIDDLEWARE_ALLOWED_METHODS', ['POST', 'GET', 'OPTIONS', 'PUT',
-                                                                                      'DELETE'])
-XHR_MIDDLEWARE_ALLOWED_HEADERS = getattr(settings, 'XHR_MIDDLEWARE_ALLOWED_HEADERS', ['Content-Type', 'Authorization',
-                                                                                      'Location', '*'])
-XHR_MIDDLEWARE_ALLOWED_CREDENTIALS = getattr(settings, 'XHR_MIDDLEWARE_ALLOWED_CREDENTIALS', 'true')
-XHR_MIDDLEWARE_EXPOSE_HEADERS = getattr(settings, 'XHR_MIDDLEWARE_EXPOSE_HEADERS', ['Location'])
+from django_extra_tools.conf import settings
 
 
 class XhrMiddleware(object):
@@ -21,20 +14,31 @@ class XhrMiddleware(object):
     def process_request(self, request):
         if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
-            response['Access-Control-Allow-Origin'] = XHR_MIDDLEWARE_ALLOWED_ORIGINS
-            response['Access-Control-Allow-Methods'] = ','.join(XHR_MIDDLEWARE_ALLOWED_METHODS)
-            response['Access-Control-Allow-Headers'] = ','.join(XHR_MIDDLEWARE_ALLOWED_HEADERS)
-            response['Access-Control-Allow-Credentials'] = XHR_MIDDLEWARE_ALLOWED_CREDENTIALS
-            response['Access-Control-Expose-Headers'] = ','.join(XHR_MIDDLEWARE_EXPOSE_HEADERS)
+            response['Access-Control-Allow-Origin'] = \
+                settings.XHR_MIDDLEWARE_ALLOWED_ORIGINS
+            response['Access-Control-Allow-Methods'] = \
+                ','.join(settings.XHR_MIDDLEWARE_ALLOWED_METHODS)
+            response['Access-Control-Allow-Headers'] = \
+                ','.join(settings.XHR_MIDDLEWARE_ALLOWED_HEADERS)
+            response['Access-Control-Allow-Credentials'] = \
+                settings.XHR_MIDDLEWARE_ALLOWED_CREDENTIALS
+            response['Access-Control-Expose-Headers'] = \
+                ','.join(settings.XHR_MIDDLEWARE_EXPOSE_HEADERS)
+
             return response
 
         return None
 
     def process_response(self, request, response):
-        response['Access-Control-Allow-Origin'] = XHR_MIDDLEWARE_ALLOWED_ORIGINS
-        response['Access-Control-Allow-Methods'] = ','.join(XHR_MIDDLEWARE_ALLOWED_METHODS)
-        response['Access-Control-Allow-Headers'] = ','.join(XHR_MIDDLEWARE_ALLOWED_HEADERS)
-        response['Access-Control-Allow-Credentials'] = XHR_MIDDLEWARE_ALLOWED_CREDENTIALS
-        response['Access-Control-Expose-Headers'] = ','.join(XHR_MIDDLEWARE_EXPOSE_HEADERS)
+        response['Access-Control-Allow-Origin'] = \
+            settings.XHR_MIDDLEWARE_ALLOWED_ORIGINS
+        response['Access-Control-Allow-Methods'] = \
+            ','.join(settings.XHR_MIDDLEWARE_ALLOWED_METHODS)
+        response['Access-Control-Allow-Headers'] = \
+            ','.join(settings.XHR_MIDDLEWARE_ALLOWED_HEADERS)
+        response['Access-Control-Allow-Credentials'] = \
+            settings.XHR_MIDDLEWARE_ALLOWED_CREDENTIALS
+        response['Access-Control-Expose-Headers'] = \
+            ','.join(settings.XHR_MIDDLEWARE_EXPOSE_HEADERS)
 
         return response
